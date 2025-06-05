21661ec8cdb256651ab0cfadc13d2187165f6fd2
suzukikeita1 <keita151105@gmail.com>
2025-06-05 14:59:40 +0900

    Riverpodを導入し依存性注入を設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Splash_Page.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_1/splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart'; // Riverpod のインポート
-import 'package:hive_flutter/hive_flutter.dart'; // Hive のインポート
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Hive を初期化して Box を開く（アプリ起動前に一度だけ）
-  await Hive.initFlutter();
-  await Hive.openBox('cache'); // 汎用キャッシュ用 Box
-
-  // Riverpod の DI ツリーを有効化
-  runApp(const ProviderScope(child: MyApp()));
+  // RiverpodのDIツリーの有効化
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -21,27 +20,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // TRY THIS: Try running your application with "flutter run". You'll see
-          // the application has a purple toolbar. Then, without quitting the app,
-          // try changing the seedColor in the colorScheme below to Colors.green
-          // and then invoke "hot reload" (save your changes or press the "hot
-          // reload" button in a Flutter-supported IDE, or press "r" if you used
-          // the command line to start the app).
-          //
-          // Notice that the counter didn't reset back to zero; the application
-          // state is not lost during the reload. To reset the state, use hot
-          // restart instead.
-          //
-          // This works for code too, not just values: Most code changes can be
-          // tested with just a hot reload.
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        //画面遷移のためのルーティング
+        title: 'MLB hitter analysis',
+        theme: ThemeData(useMaterial3: true,),
         home: SplashScreen());
   }
 }
